4258b3b463df19eb98a5c670b304dba404decb8a
Tom Simmons (Tomboyo) <tomasimmons@gmail.com>
2020-05-24 17:53:57 -0400

    init with basic spring boot

---
+++ src/main/java/com/github/tomboyo/brainstorm/brainstorm/BrainstormApplication.java
@@ -0,0 +1,13 @@
+package com.github.tomboyo.brainstorm.brainstorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainstormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainstormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/tomboyo/brainstorm/brainstorm/BrainstormApplicationTests.java
@@ -0,0 +1,12 @@
+package com.github.tomboyo.brainstorm.brainstorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainstormApplicationTests {
+
+	@Test
+	void contextLoads() {}
+
+}
