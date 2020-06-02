9fd9c3c8f44327ea95091403a5f9b1b57dbecfc2
Tom Simmons <thsimmon@redhat.com>
2020-06-01 23:43:46 -0400

    Simplify routes

--- src/main/java/com/github/tomboyo/brainstorm/routes/BrainstormRouteBuilder.java
+++ src/main/java/com/github/tomboyo/brainstorm/routes/BrainstormRouteBuilder.java
@@ -1,7 +1,5 @@
 package com.github.tomboyo.brainstorm.routes;
 
-import static java.lang.String.format;
-
 import com.github.tomboyo.brainstorm.configuration.PropertyConfig;
 import com.github.tomboyo.brainstorm.predicate.FileHasExtension;
 import com.github.tomboyo.brainstorm.processor.AdocDocumentUpdateProcessor;
@@ -33,39 +31,24 @@ public class BrainstormRouteBuilder extends RouteBuilder {
 	
 	@Override
 	public void configure() throws Exception {
-		from(format("file-watch:%s?events=CREATE,MODIFY",
-				config.notebookDirectory()))
+		fromF("file-watch:%s?events=CREATE,MODIFY",
+				config.notebookDirectory())
 			.filter(fileHasExtension)
 			.to("log:file.change?level=INFO")
-			.to("vm:file.change");
-	
-		from("vm:file.change")
 			.process(documentReferenceProcessor)
 			.to("log:file.processed?level=INFO")
-			.to("vm:graph.update");
-		
-		from("vm:graph.update")
 			.process(graphUpdateProcessor);
-
-		// TODO: split into two builders?
 
 		restConfiguration()
 			.component("netty-http")
 			.port("8080")
 			.host("localhost");
 		
-		rest("/status")
-			.get("/ok").route().transform().constant("OK");
-		
 		rest("/graph")
 			.get("/")
 				.produces("application/json")
 			.route()
 				.to("log:query?level=INFO")
-				.to("direct:query");
-		
-		from("direct:query")
-			.process(graphQueryProcessor)
-			.to("log:query?level=INFO");
+				.process(graphQueryProcessor);
 	}
 }
