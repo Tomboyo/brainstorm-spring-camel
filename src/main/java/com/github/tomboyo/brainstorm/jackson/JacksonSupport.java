2300d7d48276552a6ea440cc1517444382dd9299
Tom Simmons <thsimmon@redhat.com>
2020-06-10 21:02:21 -0400

    Serialize paths according to toString, not as URIs

---
+++ src/main/java/com/github/tomboyo/brainstorm/jackson/JacksonSupport.java
@@ -0,0 +1,55 @@
+package com.github.tomboyo.brainstorm.jackson;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class JacksonSupport {
+	private static final String groupId = "com.github.tomboyo.brainstorm";
+	private static final String artifactId = "brainstorm";
+
+	@Bean @Primary
+	public static ObjectMapper defaultObjectMapper() {
+		var mapper = new ObjectMapper();
+		mapper.registerModule(defaultModule());
+		return mapper;
+	}
+
+	private static Module defaultModule() {
+		var module = new SimpleModule(
+			"DefaultModule",
+			new Version(1, 0, 0, null, groupId, artifactId));
+		module.addSerializer(Path.class, new PathSerializer());
+		return module;
+	}
+
+	private static class PathSerializer extends StdSerializer<Path> {
+		private static final long serialVersionUID = 1L;
+		
+		private PathSerializer() {
+			super(Path.class);
+		}
+
+		@Override
+		public void serialize(
+			Path value,
+			JsonGenerator gen,
+			SerializerProvider provider
+		) throws IOException {
+			gen.writeString(value.normalize().toString());
+		}
+	}
+}
---
+++ src/test/java/com/github/tomboyo/brainstorm/jackson/JacksonSupportTest.java
@@ -0,0 +1,28 @@
+package com.github.tomboyo.brainstorm.jackson;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.nio.file.Paths;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.junit.jupiter.api.Test;
+
+public class JacksonSupportTest {
+	private static ObjectMapper mapper =
+		JacksonSupport.defaultObjectMapper();
+
+	@Test
+	public void absolutePaths() throws Exception {
+		assertEquals(
+			"\"/this/is/a/path\"",
+			mapper.writeValueAsString(Paths.get("/this/is/a/path")));
+	}
+
+	@Test
+	public void denormalizedPaths() throws Exception {
+		assertEquals(
+			"\"/this/is/a/path\"",
+			mapper.writeValueAsString(Paths.get("/this/./is/../is/a/path")));
+	}
+}
